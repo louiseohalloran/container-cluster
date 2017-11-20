3ba0b2c0eae55ff2e6e7d5d716844e68f8d13c32
louiseohalloran <louise.ohalloran@ie.ibm.com>
2017-11-20 12:38:03 +0000

    Create pod.tf

---
+++ pod.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_pod" "echo" {
+metadata {
+  name = "echo-example"
+
+labels {
+    App = "echo"
+  }
+}
+
+spec {
+  container {
+    image = "hashicorp/http-echo:0.2.1"
+    name  = "example2"
+    args = ["-listen=:80", "-text='Hello World'"]
+
+port {
+  container_port = 80
+}
+
+}
+}
+}
